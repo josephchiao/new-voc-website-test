4a1236a1cd9c5b5d0c4f0ee09c02b934ef983d23
Emily Wood <emily.wood1624@gmail.com>
2024-08-01 16:13:26 -0700

    use first_name, last_name for consistency with profile field names

--- src/ubc_voc_website/ubc_voc_website/forms.py
+++ src/ubc_voc_website/ubc_voc_website/forms.py
@@ -4,8 +4,8 @@ from .models import User
 from membership.models import Profile
 
 class CustomUserCreationForm(UserCreationForm):
-    firstname = forms.CharField(max_length=64, required=True)
-    lastname = forms.CharField(max_length=64, required=True)
+    first_name = forms.CharField(max_length=64, required=True)
+    last_name = forms.CharField(max_length=64, required=True)
     pronouns = forms.CharField(max_length=32, required=False)
     phone = forms.CharField(max_length=32, required=True)
     student_number = forms.CharField(max_length=8, required=False)
@@ -13,7 +13,7 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('email', 'firstname', 'lastname', 'pronouns', 'phone', 'student_number', 'birthdate')
+        fields = ('email', 'first_name', 'last_name', 'pronouns', 'phone', 'student_number', 'birthdate')
 
     def save(self, commit=True):
         user = super().save(commit=False)
@@ -21,8 +21,8 @@ class CustomUserCreationForm(UserCreationForm):
             user.save()
             Profile.objects.create(
                 user=user,
-                firstname=self.cleaned_data['firstname'],
-                lastname=self.cleaned_data['lastname'],
+                first_name=self.cleaned_data['first_name'],
+                last_name=self.cleaned_data['last_name'],
                 pronouns=self.cleaned_data['pronouns'],
                 phone=self.cleaned_data['phone'],
                 student_number=self.cleaned_data['student_number'],
