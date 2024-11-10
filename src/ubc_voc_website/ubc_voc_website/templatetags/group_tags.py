1e4505325df383ef64aee0ede75b5a7773ebee31
epw1624 <emily.wood1624@gmail.com>
2024-11-09 17:20:43 -0800

    fix template tags to use db data instead of django groups

--- src/ubc_voc_website/ubc_voc_website/templatetags/group_tags.py
+++ src/ubc_voc_website/ubc_voc_website/templatetags/group_tags.py
@@ -1,19 +1,31 @@
 from django import template
-from django.contrib.auth.models import Group
+from datetime import datetime
+from membership.models import Exec, Membership, PSG
 
 register = template.Library()
 
 @register.filter(name='is_member')
 def is_member(user):
-    return user.groups.filter(name="Members").exists()
+    memberships = Membership.objects.filter(user=user, end_date__gte=datetime.now())
+    if memberships.count() == 1:
+        return True
+    else:
+        return False
 
 @register.filter(name='is_exec')
 def is_exec(user):
-    return user.groups.filter(name="Exec").exists()
-
+    exec_positions = Exec.objects.filter(user=user)
+    if exec_positions.count() >= 1:
+        return True
+    else:
+        return False
 
 @register.filter(name='is_psg')
 def is_psg(user):
-    return user.groups.filter(name="PSG").exists()
+    psg_positions = PSG.objects.filter(user=user)
+    if psg_positions.count() >= 1:
+        return True
+    else:
+        return False
 
 
