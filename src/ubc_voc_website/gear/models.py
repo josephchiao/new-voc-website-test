c760d751186ff4f8fc0980723754386ae6cafc73
epw1624 <emily.wood1624@gmail.com>
2025-03-30 01:11:17 -0700

    gear app and gear hour models

---
+++ src/ubc_voc_website/gear/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.conf import settings
+
+
+class GearHour(models.Model):
+    qm = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.cascade,
+    )
+
+    """
+    Since gear hours are often recurring at the same day/time each week, this is set up as a recurring event
+    This way QMs can post their hours once per semester (or whenever they change), and they won't have to update them weekly
+
+    The downside to this approach is it requires a second model for CancelledGearHours, because there needs to be a way
+    to cancel a gear hour for one week without deleting the whole recurrence
+    """
+    start_date = models.DateTimeField()
+    end_date = models.DateField()
+    duration = models.IntegerField(default=60)
+
+class CancelledGearHour(models.Model):
+    gear_hour = models.ForeignKey(
+        GearHour,
+        on_delete=models.CASCADE,
+        related_name="cancelled_dates"
+    )
+    date = models.DateTimeField()
