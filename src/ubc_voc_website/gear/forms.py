11091f7f5ba71d94dce92cfe81344d1658a22d99
epw1624 <emily.wood1624@gmail.com>
2025-04-14 11:30:22 -0700

    forms for gear hours

---
+++ src/ubc_voc_website/gear/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from .models import GearHour, CancelledGearHour
+
+from django.contrib.auth import get_user_model
+
+import datetime
+
+User = get_user_model()
+
+class GearHourForm(forms.ModelForm):
+    class Meta:
+        model = GearHour
+        fields = (
+            'start_date',
+            'end_date',
+            'duration'
+        )
+
+    start_date = forms.DateTimeField(
+        required=True,
+        initial=datetime.datetime.now(),
+        widget=forms.TextInput(attrs={'class': 'flatpickr'})
+    )
+    end_date = forms.DateField(
+        required=True
+    )
+    duration = forms.IntegerField(
+        required=True,
+        initial=60
+    )
+
+
+class CancelledGearHourForm(forms.ModelForm):
+    class Meta:
+        model = CancelledGearHour
+        fields = (
+            'gear_hour',
+            'date'
+        )
+
+    gear_hour = forms.ModelChoiceField(
+        queryset=GearHour.objects.all(),
+        label="Gear Hour",
+        widget=forms.Select,
+        required=True
+    )
+
+    date = forms.DateField(
+        required=True
+    )
