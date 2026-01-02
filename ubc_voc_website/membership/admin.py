3a6b61f7eaef048639594326e5da2a6d91cf8cca
epw1624 <emily.wood1624@gmail.com>
2026-01-02 15:54:39 -0800

    fix profile admin menu

--- ubc_voc_website/membership/admin.py
+++ ubc_voc_website/membership/admin.py
@@ -10,7 +10,7 @@ class MembershipAdmin(admin.ModelAdmin):
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'first_name', 'last_name', 'phone')
-    search_fields = ('user',)
+    search_fields = ('user__email', 'first_name', 'last_name')
     list_filter = ('first_name', 'last_name')
 
     formfield_overrides = {
