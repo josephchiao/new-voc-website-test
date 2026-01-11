d75816397da7edf0e76d83d86cba6df5e0f7286a
epw1624 <emily.wood1624@gmail.com>
2026-01-10 20:13:50 -0800

    missing comma

--- ubc_voc_website/membership/management/commands/migrate_waiver.py
+++ ubc_voc_website/membership/management/commands/migrate_waiver.py
@@ -29,7 +29,7 @@ class Command(BaseCommand):
                 "guardian_name",
                 "studentnumber",
                 "signature",
-                "paper_waiver"
+                "paper_waiver",
                 "saved"
             ])
 
