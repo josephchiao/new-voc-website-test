41c9ab769b8ad668e0d9d07bd1ebf5e351aacbad
epw1624 <emily.wood1624@gmail.com>
2026-01-21 20:24:26 -0800

    handle conflicting post titles

--- ubc_voc_website/tripreports/management/commands/migrate_trip_reports.py
+++ ubc_voc_website/tripreports/management/commands/migrate_trip_reports.py
@@ -1,6 +1,7 @@
 """
-SELECT ID, post_author, post_date, post_content, post_title, post_name, post_status FROM `wp_posts` where post_type="post" and (post_status="draft" or post_status="pending" or post_status="publish")
+SELECT ID, post_author, post_date, post_content, post_title, post_status FROM `wp_posts` where post_type="post" and (post_status="draft" or post_status="pending" or post_status="publish") order by post_status desc
 """
+from django.core.exceptions import ValidationError
 from django.core.management import BaseCommand
 from django.contrib.auth import get_user_model
 from django.utils.text import slugify
@@ -42,27 +43,30 @@ class Command(BaseCommand):
                     continue
 
                 if TripReport.objects.filter(old_id=int(row["ID"])).exists():
-                    self.stdout.write(f"Skipping trip report with old_id {int(row["ID"])} - already exists")
+                    self.stdout.write(f"Skipping '{row["post_title"]}' - already exists")
                     continue
 
                 if not row["post_title"] or not row["post_content"]: # wagtail doesn't permit empty post content, so don't bother migrating these
                     continue
 
-                trip_report = TripReport(
-                    title=row["post_title"],
-                    slug=slugify(row["post_title"]),
-                    body=RichText(row["post_content"]),
-                    owner=user,
-                    old_id=int(row["ID"]),
-                    first_published_at=datetime.strptime(row["post_date"], "%Y-%m-%d %H:%M:%S").replace(tzinfo=pacific_timezone)
-                )
+                try:
+                    trip_report = TripReport(
+                        title=row["post_title"],
+                        slug=slugify(row["post_title"]),
+                        body=RichText(row["post_content"]),
+                        owner=user,
+                        old_id=int(row["ID"]),
+                        first_published_at=datetime.strptime(row["post_date"], "%Y-%m-%d %H:%M:%S").replace(tzinfo=pacific_timezone)
+                    )
+                except ValidationError:
+                    self.stdout.println(self.style.WARNING(f"Skipping {row["post_title"]} - post wiht this slug already exists"))
 
                 parent.add_child(instance=trip_report)
                 revision = trip_report.save_revision()
 
                 if row["post_status"] == "publish":
                     revision.publish()
-                    self.stdout.write(self.style.SUCCESS(f"Created and published {trip_report.title}"))
+                    self.stdout.write(self.style.SUCCESS(f"Created and published '{trip_report.title}'"))
                 else:
                     trip_report.live = False
                     trip_report.has_unpublished_changes = True
@@ -72,8 +76,8 @@ class Command(BaseCommand):
                         workflow = trip_report.get_workflow()
                         if workflow:
                             workflow.start(trip_report, user)
-                        self.stdout.write(self.style.SUCCESS(f"Created {trip_report.title} and submitted for approval"))
+                        self.stdout.write(self.style.SUCCESS(f"Created '{trip_report.title}' and submitted for approval"))
                     else:
-                        self.stdout.write(self.style.SUCCESS(f"Created draft {trip_report.title}"))
+                        self.stdout.write(self.style.SUCCESS(f"Created draft '{trip_report.title}'"))
 
             self.stdout.write(self.style.SUCCESS(f"Trip report migration complete"))
