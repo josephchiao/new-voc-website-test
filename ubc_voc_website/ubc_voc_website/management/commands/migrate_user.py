b29cf10972b1c0136045fff8ac385b79a4ddcb26
epw1624 <emily.wood1624@gmail.com>
2026-01-23 16:07:09 -0800

    Don't make existing members verify their email

--- ubc_voc_website/ubc_voc_website/management/commands/migrate_user.py
+++ ubc_voc_website/ubc_voc_website/management/commands/migrate_user.py
@@ -1,7 +1,9 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from django.utils.crypto import get_random_string
+
 import csv
+from allauth.account.models import EmailAddress
 
 User = get_user_model()
 
@@ -28,5 +30,19 @@ class Command(BaseCommand):
                     self.stdout.write(self.style.SUCCESS(f"Created user {user.email}"))
                 else:
                     self.stdout.write(f"User {user.email} already exists")
+
+                email_obj, email_created = EmailAddress.objects.get_or_create(
+                    user=user,
+                    email=user.email,
+                    defaults={
+                        "verified": True,
+                        "primary": True
+                    }
+                )
+
+                if not email_created:
+                    email_obj.verified = True
+                    email_obj.primary = True
+                    email_obj.save()
 
         self.stdout.write(self.style.SUCCESS("User migration complete"))        
