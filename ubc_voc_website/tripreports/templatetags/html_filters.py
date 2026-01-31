ccc033cf458c94f5b7004f20de024722be403584
epw1624 <emily.wood1624@gmail.com>
2026-01-30 16:15:25 -0800

    Don't show images in trip report preview card

---
+++ ubc_voc_website/tripreports/templatetags/html_filters.py
@@ -0,0 +1,10 @@
+import re
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter
+def strip_images(value):
+    img_stripped = re.sub(r'<img[^>]*>', '', value)
+    return mark_safe(img_stripped)
